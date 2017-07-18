44bd7ee95d8c05cdc41ead366951064425a7154a
Adam Howard <achdiposable1@gmail.com>
2017-07-18 11:14:15 +0100

    tweaked layout, and renamed activity to Status Report

--- app/src/main/java/com/medavox/diabeticdiary/StatusReportActivity.java
+++ app/src/main/java/com/medavox/diabeticdiary/StatusReportActivity.java
@@ -3,10 +3,10 @@ package com.medavox.diabeticdiary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ReviewEntriesActivity extends AppCompatActivity {
+public class StatusReportActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review_entries);
+        setContentView(R.layout.activity_status_report);
     }
 }
