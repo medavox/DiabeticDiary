3b5eb83884ca37bb42cebb09ad062d77deef3e1e
Adam Howard <achdiposable1@gmail.com>
2017-09-01 09:38:36 +0100

    started work on ingredients carb calculator

---
+++ app/src/main/java/com/medavox/diabeticdiary/IngredientCarbCalculator.java
@@ -0,0 +1,13 @@
+package com.medavox.diabeticdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IngredientCarbCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_carb_calculator);
+    }
+}
