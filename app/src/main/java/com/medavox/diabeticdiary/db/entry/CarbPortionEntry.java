e4b2603b0c6c558582ae64ced8fca2d315c6d4db
Adam Howard <achdiposable1@gmail.com>
2017-09-06 13:13:09 +0100

    hopefully fixed bug with imprecise floating-point maths on CP entries

--- app/src/main/java/com/medavox/diabeticdiary/db/entry/CarbPortionEntry.java
+++ app/src/main/java/com/medavox/diabeticdiary/db/entry/CarbPortionEntry.java
@@ -6,16 +6,17 @@ package com.medavox.diabeticdiary.db.entry;
  */
 
 public class CarbPortionEntry extends Entry {
-    private float cp;
+    private int carbGrams;
     public CarbPortionEntry(String cp, long time) throws NumberFormatException {
         super(time);
         if(!isValid(cp, 3, 1)) {
             throw new NumberFormatException("Invalid Carb Portion passed to constructor:\""+cp+"\"");
         }
-        this.cp = Float.parseFloat(cp);
+        float cpFloat = Float.parseFloat(cp);
+        this.carbGrams = (int)(cpFloat * 10);
     }
 
     public float getCarbPortion() {
-        return cp;
+        return ((float) carbGrams) / 10;
     }
 }
