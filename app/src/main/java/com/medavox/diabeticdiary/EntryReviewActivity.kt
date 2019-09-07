ac2b4ecee9391c919d8cdfb049c31863378e69fa
Adam Howard <achdisposable1+github@gmail.com>
2019-09-07 22:47:37 +0100

    feat: implement Entry Review
    for some reason nothing is being displayed,
    but the bulk of the work has been done

--- app/src/main/java/com/medavox/diabeticdiary/EntryReviewActivity.kt
+++ app/src/main/java/com/medavox/diabeticdiary/EntryReviewActivity.kt
@@ -1,12 +1,18 @@
 package com.medavox.diabeticdiary
 
-import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.medavox.diabeticdiary.db.EntryDao
+import kotlinx.android.synthetic.main.entry_review_item.view.*
+import org.threeten.bp.*
+import org.threeten.bp.format.DateTimeFormatter
 
 /**
  * @author Adam Howard
@@ -24,30 +30,84 @@ class EntryReviewActivity : AppCompatActivity() {
         recyclerView = findViewById<RecyclerView>(R.id.entry_review_list).apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
-            setHasFixedSize(true)
+            setHasFixedSize(false)
 
             // use a linear layout manager
-            layoutManager = LinearLayoutManager(this@EntryReviewActivity)
+            layoutManager = LinearLayoutManager(this@EntryReviewActivity)//.apply{reverseLayout = true}
 
             // specify an viewAdapter (see also next example)
-            adapter = EntryAdapter(resources)
+            adapter = EntryAdapter(DiabApp.db())
         }
     }
 
-    data class EntryViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    data class EntryViewHolder(val view:View) : RecyclerView.ViewHolder(view){
+        fun reuse(entryValue:String, dateHeading:String?=null, timeHeading:String?=null) {
+            view.entry_value.text = entryValue
+            if(dateHeading != null) {
+                view.date_heading.text = dateHeading
+                view.date_heading.visibility = View.VISIBLE
+            } else {
+                view.visibility = View.GONE
+            }
+            if(timeHeading != null) {
+                view.time_heading.text = timeHeading
+                view.time_heading.visibility = View.VISIBLE
+            } else {
+                view.visibility = View.GONE
+            }
+        }
+    }
 
-    inner class EntryAdapter(private val res: Resources)
-        : RecyclerView.Adapter<EntryViewHolder>() {
+    //TODO: Use those context-establishing time/date headings (like on slack/fbmsgr)
+    // preceding each entry:
+    // 1) before the first entry on a date different to the previous entry, and
+    // 2) on a different time (hour and minute) to the previous entry
+    // considering we don't control individual Views placement (the RecyclerView does),
+    // implement this by including the date or time heading as part of that entry with the different date/time
+    inner class EntryAdapter(private val dao:EntryDao): RecyclerView.Adapter<EntryViewHolder>() {
+        private val TAG = "EntryAdapter"
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryViewHolder {
-            TODO("not implemented")
+            val v = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.entry_review_item, parent, false) as LinearLayout
+            return EntryViewHolder(v)
         }
 
         override fun getItemCount(): Int {
-            TODO("not implemented")
+            val num = dao.getNumberOfEntries()
+            Log.v(TAG, "entries: $num")
+            return num
         }
 
         override fun onBindViewHolder(holder: EntryViewHolder, position: Int) {
-            TODO("not implemented")
+            val entry = dao.getNthMostRecentEntry(position)
+            if(entry == null) {
+                Log.e(TAG, "entry $position is null!")
+                return
+            }
+            Log.v(TAG, "entry:$entry")
+            val previousEntry = dao.getNthMostRecentEntry(position-1)
+
+            val currentDateTime = LocalDateTime.ofInstant(Instant.
+                    ofEpochMilli(entry.time), ZoneOffset.UTC)
+            data class showDateAndTime(val showDate:Boolean, val showTime:Boolean)
+
+            val (showDate, showTime) = if(previousEntry == null) {
+                showDateAndTime(showDate = true, showTime = true)
+            }else {
+                val previousDateTime = LocalDateTime.ofInstant(Instant.
+                        ofEpochMilli(previousEntry.time), ZoneOffset.UTC)
+                showDateAndTime(
+                currentDateTime.toLocalDate() == previousDateTime.toLocalDate(),
+                currentDateTime.toLocalTime() == previousDateTime.toLocalTime()
+                )
+            }
+            val dtfDate:DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM")
+            val dtfTime:DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+            return holder.reuse(
+                entryValue = entry.data,
+                dateHeading = if(showDate) null else currentDateTime.toLocalDate().format(dtfDate),
+                timeHeading = if(showTime) null else currentDateTime.toLocalTime().format(dtfTime)
+            )
         }
     }
 }
