1050a288cc1e0b5fc8539315743600ab08a89d6e
Adam Howard <achdisposable1+github@gmail.com>
2019-09-07 09:41:30 +0100

    refactor(EntryReviewActivity): remove cruft

--- app/src/main/java/com/medavox/diabeticdiary/EntryReviewActivity.kt
+++ app/src/main/java/com/medavox/diabeticdiary/EntryReviewActivity.kt
@@ -2,9 +2,6 @@ package com.medavox.diabeticdiary
 
 import android.content.res.Resources
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -17,29 +14,23 @@ import androidx.recyclerview.widget.RecyclerView
  */
 class EntryReviewActivity : AppCompatActivity() {
 
-    private lateinit var listView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
-    private val uiThreadHandler = Handler(Looper.getMainLooper())
-    internal var bottleCommsActive:Boolean = false
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry_review)
 
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = EntryAdapter(resources)
         // Inflate the layout for this fragment
-        listView = findViewById<RecyclerView>(R.id.entry_review_list).apply {
+        recyclerView = findViewById<RecyclerView>(R.id.entry_review_list).apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
 
             // use a linear layout manager
-            layoutManager = viewManager
+            layoutManager = LinearLayoutManager(this@EntryReviewActivity)
 
             // specify an viewAdapter (see also next example)
-            adapter = viewAdapter
+            adapter = EntryAdapter(resources)
         }
     }
 
@@ -48,15 +39,15 @@ class EntryReviewActivity : AppCompatActivity() {
     inner class EntryAdapter(private val res: Resources)
         : RecyclerView.Adapter<EntryViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryViewHolder {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            TODO("not implemented")
         }
 
         override fun getItemCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            TODO("not implemented")
         }
 
         override fun onBindViewHolder(holder: EntryViewHolder, position: Int) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            TODO("not implemented")
         }
     }
 }
